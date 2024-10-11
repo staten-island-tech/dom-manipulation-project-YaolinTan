67a4fbeb05ebb40134b9acdce903c82aa4aedba3
YaolinTan <yaolint@nycstudents.net>
2024-10-11 12:26:45 -0400

    e

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+const DOMSelectors = {
+  header: document.querySelector("h1"),
+  items: document.querySelectorAll("li"),
+  button: document.querySelector(".btn"),
+  text: document.querySelector("#text"),
+  container: document.querySelector(".container"),
+};
